4e941dc17b033ceb87bd58cc4fa0a6222970c3b4
ShubhamGupta <92706349+ShubhamGupta2701@users.noreply.github.com>
2024-03-27 00:50:35 +0530

    Update index.js
    create a http server 
    use fs module 
    create a simple log file using fs module to capture request info

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const math = require("./math");
+// const math = require("./math");
 
-console.log(math);
-console.log("sum of 2 numbers 5 and 8 is : ",math.add(5,8));
+// console.log(math);
+// console.log("sum of 2 numbers 5 and 8 is : ",math.add(5,8));
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    const log =`${Date.now()} : New request Recieved\n`;
+    fs.appendFile("log.txt",log,(err,data)=>{
+        res.end("hello from server");
+    });
+});
+
+server.listen(3000,()=>{console.log("server started")});
